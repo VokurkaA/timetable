cf0967b81e82683b48eb37519271dc33ec5212fe
zadrobilek.jan@seznam.cz <zadrobilek.jan@seznam.cz>
2023-12-20 11:44:26 +0100

    styling changes

--- bakalari_login.js
+++ bakalari_login.js
@@ -73,6 +73,8 @@ async function creteForLoginForm() {
     changeBgBox.style.backgroundImage = ('linear-gradient(to right, #514A9D, #24C6DC)');
     changeBgBox.style.border = 'none';
     changeBgBox.style.cursor = 'pointer';
+    changeBgBox.style.borderRadius = '1vw';
+    changeBgBox.marginTop = '0.5vw';
     loginForm.appendChild(changeBgBox);
 
     changeBgBox.onclick = function(){
