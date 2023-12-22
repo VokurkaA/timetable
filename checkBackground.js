e133400b6a563946a4027572ed4aaf0ffcc86da1
zadrobilek.jan@seznam.cz <zadrobilek.jan@seznam.cz>
2023-12-22 16:08:57 +0100

    changes

--- checkBackground.js
+++ checkBackground.js
@@ -4,4 +4,5 @@ if (cookies.length == 4){
     var bg = cookies[3].split("=");
     document.body.style.background = bg[1];
     document.body.style.backgroundRepeat =  "no-repeat";
+    document.body.style.backgroundAttachment = "fixed";
 }
