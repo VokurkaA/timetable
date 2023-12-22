fc1537b47258c2ba64f4a2ef7361748ffe102002
zadrobilek.jan@seznam.cz <zadrobilek.jan@seznam.cz>
2023-12-22 16:07:56 +0100

    changes

--- checkBackground.js
+++ checkBackground.js
@@ -3,4 +3,5 @@ cookies = cookies.split("; ");
 if (cookies.length == 4){
     var bg = cookies[3].split("=");
     document.body.style.background = bg[1];
+    document.body.style.backgroundRepeat =  "no-repeat";
 }
