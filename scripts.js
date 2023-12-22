5b5be4a5ab42d1541769a2f5a8dd8746b18e9d45
zadrobilek.jan@seznam.cz <zadrobilek.jan@seznam.cz>
2023-12-22 16:01:39 +0100

    fr this time

--- scripts.js
+++ scripts.js
@@ -8,7 +8,8 @@ function checkBackground() {
     var cookies = document.cookie;
     cookies = cookies.split("; ");
     if (cookies.length == 4){
-        document.body.style.background = cookies[3];
+        var bg = cookies[3].split("=");
+        document.body.style.background = bg[1];
     }
 }
 function printLogin() {
